491af54157a004ff35371a1b6e493fd80221e7c1
YUEN-YU-WING <henryyuen200241@gmail.com>
2022-02-11 20:11:54 +0800

    final

--- src/app/App.js
+++ src/app/App.js
@@ -1,10 +1,7 @@
 import './App.css';
 import React from 'react';
-// Import Video.js and Nenu.js
 import { Menu } from '../menu/Menu';
 import { Video } from '../video/Video';
-
-/* 1. Click Save, and take a look at your video player in the browser. It looks pretty good! But if you try interacting with it, you’ll find that there’s zero functionality. Take a look at the App component class. This class has one property stored as state: a src containing the address of a video file. App‘s job is to pass this src down to a stateless component, and to pass the ability to change the src down to a different stateless component. Passing src is the easier part, so let’s do that first. Inside of App‘s render function, give <Video /> an attribute. Make this attribute’s name src, and the attribute’s value equal to the src property stored in this.state. */
 
 const VIDEOS = {
   fast: 'https://content.codecademy.com/courses/React/react_video-fast.mp4',
@@ -12,25 +9,17 @@ const VIDEOS = {
   cute: 'https://content.codecademy.com/courses/React/react_video-cute.mp4',
   eek: 'https://content.codecademy.com/courses/React/react_video-eek.mp4'
 };
-
-//alert("hi");
-/* If you pass chooseVideo to <Menu />, then you will give <Menu /> the ability to update <App />‘s state.
-
-In App‘s render function , give Menu a chooseVideo attribute. Set chooseVideo‘s value equal to the chooseVideo function. */
-
 class App extends React.Component {
-  /*  In the constructor of App, bind .chooseVideo() to the current value of this and store it in this.chooseVideo. */
   constructor(props) {
     super(props);
     this.chooseVideo = this.chooseVideo.bind(this);
     this.state = {
-      src: VIDEOS.fast
+      src: VIDEOS.fast,
     };
-    
   }
 
   chooseVideo(newVideo) {
-    this.setState({ src: VIDEOS[newVideo]} )
+    this.setState({ src: VIDEOS[newVideo]});
   }
 
   render() {
@@ -43,13 +32,5 @@ class App extends React.Component {
     )
   }
 }
-
-/* Give App a new property named chooseVideo. Set chooseVideo‘s value equal to a function with one parameter, named newVideo. chooseVideo is going to get passed a string: either 'fast', 'slow', 'cute', or 'eek'. It will use this string to choose a new src, which it will use to update this.state.src. */
-
-
-// ReactDOM.render(
-//   <App />, 
-//   document.getElementById('app')
-// );
 
 export default App;
--- src/menu/Menu.js
+++ src/menu/Menu.js
@@ -1,11 +1,4 @@
 import React from "react";
-
-
-/* In Menu‘s render function, give <form></form> an onClick attribute. Set onClick‘s value equal to the passed-in chooseVideo function. */
-
-/* chooseVideo is going to get passed a string: either 'fast', 'slow', 'cute', or 'eek'. It will use this string to choose a new src, which it will use to update this.state.src. */
-
-
 export class Menu extends React.Component {
     constructor(props) {
         super(props);
